6a4b801f6064c2545d46c5fcba70a61f2d66b995
Wageesh Arya <32549605+WageeshArya@users.noreply.github.com>
2020-07-05 03:38:36 +0530

    desktop navbar complete

--- src/components/layout/landing/Home.js
+++ src/components/layout/landing/Home.js
@@ -5,7 +5,38 @@ export const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="home">grs
+      <div className="home">
+        <div className="group-1">
+          <div className="branding">
+            <div className="brandName">
+              The
+              <br/>
+              Sole
+              <br/>
+              Store
+            </div>
+            <div>
+                <p>The finest designs and fits.</p>
+            </div>
+          </div>
+          <div className="viewProducts">
+            <div>
+              <p>
+                Check out our latest and greatest models
+              </p>
+              <a className="productsBtn" href="#">GO <i class="fas fa-arrow-right"/></a>
+            </div>
+          </div>
+        </div>
+        <div className="group-2">
+          <div className="products">
+            <div className="product"></div>
+            <div className="product"></div>
+            <div className="product"></div>
+            <div className="product"></div>
+          </div>
+          <div class="something"></div>
+        </div>
       </div>
     </div>
   )
--- src/components/layout/landing/Navbar.js
+++ src/components/layout/landing/Navbar.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './Navbar.css';
-
+import logo from '../../../icons/logo.png';
 import login from '../../../icons/login.png';
 import signup from '../../../icons/signup.png';
 import home from '../../../icons/home.svg';
 
 export const Navbar = () => {
+
+  const [homeHovered, setHomeHovered] = useState(false);
+
+  const setHovered = () => {
+    setHomeHovered(!homeHovered);
+    console.log(homeHovered);
+  }
+  
   return (
-    <div className="navbar">
-      <div className="brand">
-        <div>The</div>
-        <div>Sole</div>
-        <div>Store</div>
-      </div>
+    <div className={`navbar ${homeHovered ? 'extendNav' : ''}`}>
+        <div>
+          <a href="#"><img src={logo}  className="brand" alt="logo"/></a>
+        </div>
       <ul className="forUsers">
         <div>
-          <li><a href="#"><img src={home} alt=""/></a></li>
+          <div className="homeBtn">
+            <li className ={homeHovered ? 'extendHomeIcon' : ''}><a href="#"><img src={home} onClick={setHovered} alt=""/></a></li>
+            <div className={homeHovered ? 'extendHome' : 'reveal'}>
+              <ul>
+                <li><a href="#">Admin login</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div className="icons">
           <li><a href="#"><img src={login} alt="Log in"/></a></li>
